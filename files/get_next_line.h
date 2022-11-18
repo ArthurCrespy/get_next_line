ace5ce609a1acf18553ca81a0b47ab1c1d64d8fe
CRESPY Arthur <arthur@crespy.eu>
2022-11-18 10:47:40 +0100

    Add get next line header

---
+++ files/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 09:12:16 by acrespy           #+#    #+#             */
+/*   Updated: 2022/11/15 09:12:17 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+
+#endif
