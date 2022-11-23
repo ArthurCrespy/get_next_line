351ec16fb7fc6783a509f39dc657d8d94d75d84f
CRESPY Arthur <arthur@crespy.eu>
2022-11-23 12:23:25 +0100

    Upload get_next_line and his utils

--- files/get_next_line.c
+++ files/get_next_line.c
@@ -12,22 +12,95 @@
 
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+char	*ft_free(char *buffer, char *buf)
 {
-	int     nb_bytes;
-//	char    *buffer;
-	char    buffer[] = "Salut, c'est moi";
+	char	*temp;
 
-//	buffer = malloc(10 * sizeof(char));
-	nb_bytes = read(fd, buffer, 10);
-	printf("%d", nb_bytes);
-
-	return ((char *)"bonjour");
+	temp = ft_strjoin(buffer, buf);
+	free(buffer);
+	return (temp);
 }
 
-int     main(void)
+char	*ft_next(char *buffer)
 {
-	printf("%s", get_next_line(10));
+	int		i;
+	int		j;
+	char	*line;
 
-	return (0);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buffer[i])
+		line[j++] = buffer[i++];
+	free(buffer);
+	return (line);
+}
+
+char	*ft_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	line = ft_calloc(ft_strlen(buffer) + 1, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*read_file(int fd, char *result)
+{
+	char	*buffer;
+	int		byte_read;
+
+	if (!result)
+		result = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, buffer, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[byte_read] = '\0';
+		result = ft_free(result, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	free(buffer);
+	return (result);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = read_file(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_line(buffer);
+	buffer = ft_next(buffer);
+	return (line);
 }
