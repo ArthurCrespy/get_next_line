e4027c7403c259bc71596d5fcc6e59d9761c7e8f
CRESPY Arthur <arthur@crespy.eu>
2022-11-25 11:32:18 +0100

    Rearrange prototypes and add missings

--- files/get_next_line.h
+++ files/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 09:12:16 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/23 12:49:50 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/11/24 14:18:54 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *string, int c);
+char	*read_file(int fd, char *result);
+char	*ft_line(char *buffer);
+char	*ft_next(char *buffer);
+char	*ft_free(char *result, char *buffer);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
-size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
