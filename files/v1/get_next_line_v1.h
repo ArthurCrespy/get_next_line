45bdbc55b4a84ec8cabb5d5807b32855946b1c52
CRESPY Arthur <arthur@crespy.eu>
2022-12-22 06:59:24 +0100

    Add v1 project

---
+++ files/v1/get_next_line_v1.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 09:12:16 by acrespy           #+#    #+#             */
+/*   Updated: 2022/11/15 09:12:17 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(const char *str);
+char	*ft_free(char *buffer, char *buf);
+char	*ft_next(char *buffer);
+char	*ft_line(char *buffer);
+char	*read_file(int fd, char *result);
+char    *get_next_line(int fd);
+
+#endif
---
+++ files/v1/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 14:55:05 by acrespy           #+#    #+#             */
+/*   Updated: 2022/12/13 17:25:15 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../v2/get_next_line_v2.h"
+
+int	main(void)
+{
+	int		i;
+	int		fd;
+	char	*result;
+
+	i = 1;
+	fd = open("../../tests/test3", O_RDONLY);
+	while (i <= 100)
+	{
+		result = get_next_line(fd);
+		printf("%d: %s", i, result);
+		i++;
+		free(result);
+	}
+	close(fd);
+	return (0);
+}
