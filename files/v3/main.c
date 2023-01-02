7a9ac66de0ff0505dfdb0aab8077bde5cd426ae7
CRESPY Arthur <arthur@crespy.eu>
2023-01-02 07:31:54 +0100

    Add main for v3/

---
+++ files/v3/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/09 14:55:05 by acrespy           #+#    #+#             */
+/*   Updated: 2022/12/13 17:25:15 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_v3.h"
+
+int	main(void)
+{
+	int		i;
+	int		fd;
+	char	*result;
+
+	i = 1;
+	fd = open("/mnt/nfs/homes/acrespy/42/42Cursus/42Projets/get_next_line/tests/test3", O_RDONLY);
+	while (i <= 100)
+	{
+		result = get_next_line(3);
+		printf("%d: %s\n", i, result);
+		i++;
+	//	free(result);
+	}
+	close(fd);
+	return (0);
+}
