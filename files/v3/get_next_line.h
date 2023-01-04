82028be6b39ca62a8dd91679bc30bbe3bf70dab6
CRESPY Arthur <arthur@crespy.eu>
2023-01-04 15:15:27 +0100

    Norminette issues

--- files/v3/get_next_line.h
+++ files/v3/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 09:12:16 by acrespy           #+#    #+#             */
-/*   Updated: 2022/12/16 23:10:15 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/01/04 14:55:55 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *str, int c);
-char    *ft_read(int fd);
+char	*ft_read(int fd);
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 #endif
