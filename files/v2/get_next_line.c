e811d2f37d13fe7a46a43a4eb0b417c585c6c269
CRESPY Arthur <arthur@crespy.eu>
2023-01-04 15:14:36 +0100

    Fix ft_line

--- files/v2/get_next_line.c
+++ files/v2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 09:12:07 by acrespy           #+#    #+#             */
-/*   Updated: 2022/12/16 19:54:15 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/01/04 15:00:07 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,50 +24,27 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
-char	*ft_strdup(const char *s)
-{
-	int		i;
-	char	*result;
-
-	i = 0;
-	if (!s)
-		return (ft_calloc(1, 1));
-	result = ft_calloc(ft_strlen(s) + 1, sizeof(char));
-	if (!result)
-		return (NULL);
-	while (i <= (int)ft_strlen(s))
-	{
-		result[i] = s[i];
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
-}
-
 char	*ft_line(int fd, char **buffer)
 {
-	long		nb_bytes;
+	long	nb_bytes;
 	char	*result;
 
-	nb_bytes = 1;
+	nb_bytes = 0;
 	result = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (!result)
 		return (NULL);
-	while (!ft_fromchr(result, '\n'))
+	while (!ft_untilchr(*buffer, '\n'))
 	{
 		nb_bytes = read(fd, result, BUFFER_SIZE);
-		if (nb_bytes > 0)
+		if (nb_bytes == 0)
+			break ;
+		else if (nb_bytes > 0)
 		{
 			result[nb_bytes] = '\0';
 			*buffer = ft_strjoin(*buffer, result);
 		}
 		else if (nb_bytes < 0)
-		{
-			free(result);
 			return (NULL);
-		}
-		else
-			break;
 	}
 	free(result);
 	return (*buffer);
@@ -76,12 +53,14 @@ char	*ft_line(int fd, char **buffer)
 char	*get_next_line(int fd)
 {
 	char		*result;
-	char        *line;
+	char		*line;
 	static char	*buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = ft_line(fd, &buffer);
+	if (!line)
+		return (NULL);
 	result = ft_untilchr(line, '\n');
 	buffer = ft_fromchr(line, '\n');
 	free(line);
