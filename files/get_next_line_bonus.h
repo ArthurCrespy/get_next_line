03b4d03f7df4f88f4db752ea1f99f7c20b3b27be
CRESPY Arthur <arthur@crespy.eu>
2023-01-05 18:03:47 +0100

    Add bonuses

---
+++ files/get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 09:12:16 by acrespy           #+#    #+#             */
+/*   Updated: 2023/01/04 15:08:11 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_free(char *buffer, char *buf);
+char	*ft_next(char *buffer);
+char	*ft_line(char *buffer);
+char	*read_file(int fd, char *result);
+char	*get_next_line(int fd);
+
+#endif
---
+++ files/get_next_line_utils_bonus.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 09:12:02 by acrespy           #+#    #+#             */
+/*   Updated: 2023/01/04 15:07:11 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *string, int c)
+{
+	char	*str;
+
+	str = (char *)string;
+	while (*str != c && *str != '\0')
+		str++;
+	if (*str == c)
+		return (str);
+	else
+		return (NULL);
+}
+
+char	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	i;
+	char	*result;
+
+	i = 0;
+	if ((size != 0 && nmemb != 0) && ((nmemb * size) / size != nmemb))
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	result = malloc(nmemb * size);
+	if (!result)
+		return (NULL);
+	while (i < nmemb * size)
+	{
+		result[i] = '\0';
+		i++;
+	}
+	return (result);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i_s1;
+	int		i_s2;
+	char	*result;
+
+	i_s1 = 0;
+	i_s2 = 0;
+	result = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	if (s1)
+	{
+		while (s1[i_s1])
+		{
+			result[i_s1] = s1[i_s1];
+			i_s1++;
+		}
+	}
+	while (s2[i_s2])
+	{
+		result[i_s1 + i_s2] = s2[i_s2];
+		i_s2++;
+	}
+	result[i_s1 + i_s2] = '\0';
+	return (result);
+}
